105718f34bbe1a153834e1efde165b2f293b8933
Sarthak Singh <sarthaksingh0900@gmail.com>
2022-02-08 10:36:59 +0530

    Resolved issues with the ports

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const app = express();
-const port = process.env.port || 5000;
 const route = require("./routes/getRoute");
 const pingRoute = require("./routes/ping");
 app.use("/getScreenshot",route);
